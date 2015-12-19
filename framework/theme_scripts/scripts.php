a5add988e2213cf922e6b6cc513fbdd454968ae9
Meilan Lin <meilan.dk@gmail.com>
2015-12-19 15:37:28 +0100

    Add my custom js file

--- framework/theme_scripts/scripts.php
+++ framework/theme_scripts/scripts.php
@@ -1,13 +1,13 @@
 <?php
-	
+
 	//=============================================================
 	//Theme Scripts
 	//=============================================================
-	
+
 	//Register JS Scripts for later use
 if ( ! function_exists( 'shophistic_lite_enqueue_scripts' ) ){
 	function shophistic_lite_enqueue_scripts() {
-		
+
 		//hoverIntent Plugin ==============================================
 		wp_register_script('hoverIntent', SHOPHISTIC_JS . '/jquery.hoverIntent.js', array('jquery'), '6.0', true );
 		wp_enqueue_script('hoverIntent');
@@ -22,22 +22,22 @@ if ( ! function_exists( 'shophistic_lite_enqueue_scripts' ) ){
 		wp_register_script('modernizr', SHOPHISTIC_JS . '/modernizr.custom.67069.js', '2.8.3', true );
 		wp_enqueue_script('modernizr');
 		//=================================================================
-		
+
 		//Owl Carousel ========================================================
 		wp_register_script('owl', SHOPHISTIC_JS . '/owl.carousel.js', array('jquery'), '2.0.0', true );
 		wp_enqueue_script('owl');
 		//=================================================================
-		
+
 		//Pace  =============================================
 		wp_register_script('pace', SHOPHISTIC_JS . '/pace.js', array(), '0.2.0', true);
 		wp_enqueue_script('pace');
 		//=================================================================
-		
+
 		//Bootstrap JS ========================================
 		wp_register_script('bootstrap', SHOPHISTIC_JS . '/bootstrap.js', array(), '2.1.0', true);
 		wp_enqueue_script('bootstrap');
 		//=================================================================
-		
+
 		//Comment Reply ===================================================
 		if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 		//=================================================================
@@ -45,11 +45,17 @@ if ( ! function_exists( 'shophistic_lite_enqueue_scripts' ) ){
 
 
 
-		
+
 		//Customs Scripts =================================================
 		wp_register_script('theme-custom', SHOPHISTIC_JS . '/script.js', array('jquery', 'bootstrap'), '1.0', true );
 		wp_enqueue_script('theme-custom');
 		//=================================================================
+
+		//Meilan's Customs Scripts =================================================
+		wp_register_script('meilan-custom', SHOPHISTIC_JS . '/custom-js.js', array('jquery', 'bootstrap'), '1.0', true );
+		wp_enqueue_script('meilan-custom');
+		//=================================================================
+
 	}
 }//end if function_exists
 	add_action('wp_enqueue_scripts', 'shophistic_lite_enqueue_scripts');
@@ -69,7 +75,7 @@ if ( ! function_exists( 'shophistic_lite_customizer_live_preview' ) ){
 		wp_enqueue_script( 'ql-themecustomizer',			//Give the script an ID
 			  SHOPHISTIC_JS.'/theme-customizer.js',//Point to file
 			  array( 'jquery','customize-preview' ),	//Define dependencies
-			  '',						//Define a version (optional) 
+			  '',						//Define a version (optional)
 			  true						//Put script in footer?
 		);
 	}
--- js/custom-js.js
+++ js/custom-js.js
@@ -1,3 +1,18 @@
 /**
  * Meilan's own javascript
  */
+
+(function($) {
+  "use strict";
+
+	jQuery(document).ready(function($){
+
+
+		$('.ql_cart-btn').click(function (e) {
+			location.href = $(e.currentTarget).attr('href');
+		});
+
+
+	});
+
+})(jQuery);
